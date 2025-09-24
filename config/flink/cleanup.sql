1ef0da6dd2f4c7faaa19a4f42cf11100cdcb0434
Enrique Gama <147114085+kiketheone82@users.noreply.github.com>
2025-09-23 18:38:42 -0600

    update retention 2d

--- config/flink/cleanup.sql
+++ config/flink/cleanup.sql
@@ -16,19 +16,19 @@ ALTER TABLE telematics_maxtrack_raw_dlq  EXECUTE optimize(file_size_threshold =>
 ALTER TABLE telematics_queclink_raw_dlq  EXECUTE optimize(file_size_threshold => '128MB');
 ALTER TABLE telematics_suntech_raw_dlq   EXECUTE optimize(file_size_threshold => '128MB');
 
-ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw"     EXECUTE remove_orphan_files(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_queclink_raw"     EXECUTE remove_orphan_files(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_suntech_raw"      EXECUTE remove_orphan_files(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw_dlq" EXECUTE remove_orphan_files(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_queclink_raw_dlq" EXECUTE remove_orphan_files(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_suntech_raw_dlq"  EXECUTE remove_orphan_files(retention_threshold => '7d');
+ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw"     EXECUTE remove_orphan_files(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_queclink_raw"     EXECUTE remove_orphan_files(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_suntech_raw"      EXECUTE remove_orphan_files(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw_dlq" EXECUTE remove_orphan_files(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_queclink_raw_dlq" EXECUTE remove_orphan_files(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_suntech_raw_dlq"  EXECUTE remove_orphan_files(retention_threshold => '2d');
 
-ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw"     EXECUTE expire_snapshots(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_queclink_raw"     EXECUTE expire_snapshots(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_suntech_raw"      EXECUTE expire_snapshots(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw_dlq" EXECUTE expire_snapshots(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_queclink_raw_dlq" EXECUTE expire_snapshots(retention_threshold => '7d');
-ALTER TABLE "nessie"."telematics"."telematics_suntech_raw_dlq"  EXECUTE expire_snapshots(retention_threshold => '7d');
+ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw"     EXECUTE expire_snapshots(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_queclink_raw"     EXECUTE expire_snapshots(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_suntech_raw"      EXECUTE expire_snapshots(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_maxtrack_raw_dlq" EXECUTE expire_snapshots(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_queclink_raw_dlq" EXECUTE expire_snapshots(retention_threshold => '2d');
+ALTER TABLE "nessie"."telematics"."telematics_suntech_raw_dlq"  EXECUTE expire_snapshots(retention_threshold => '2d');
 
 ANALYZE telematics_maxtrack_raw;
 ANALYZE telematics_queclink_raw;
@@ -41,9 +41,9 @@ ANALYZE telematics_suntech_raw_dlq;
 
 ALTER TABLE nessie.telematics.risk_score_daily          EXECUTE optimize(file_size_threshold => '128MB') WHERE received_day = current_date - INTERVAL '1' day;
 
-ALTER TABLE "nessie"."telematics"."risk_score_daily"    EXECUTE remove_orphan_files(retention_threshold => '7d');
+ALTER TABLE "nessie"."telematics"."risk_score_daily"    EXECUTE remove_orphan_files(retention_threshold => '2d');
 
-ALTER TABLE "nessie"."telematics"."risk_score_daily"    EXECUTE expire_snapshots(retention_threshold => '7d');
+ALTER TABLE "nessie"."telematics"."risk_score_daily"    EXECUTE expire_snapshots(retention_threshold => '2d');
 
 ANALYZE nessie.telematics.risk_score_daily;
 
@@ -51,8 +51,8 @@ ANALYZE nessie.telematics.risk_score_daily;
 
 ALTER TABLE nessie.telematics.telematics_real_time          EXECUTE optimize(file_size_threshold => '256MB') WHERE received_day = current_date - INTERVAL '1' day;
 
-ALTER TABLE "nessie"."telematics"."telematics_real_time"    EXECUTE remove_orphan_files(retention_threshold => '7d');
+ALTER TABLE "nessie"."telematics"."telematics_real_time"    EXECUTE remove_orphan_files(retention_threshold => '2d');
 
-ALTER TABLE "nessie"."telematics"."telematics_real_time"    EXECUTE expire_snapshots(retention_threshold => '7d');
+ALTER TABLE "nessie"."telematics"."telematics_real_time"    EXECUTE expire_snapshots(retention_threshold => '2d');
 
 ANALYZE nessie.telematics.telematics_real_time;
